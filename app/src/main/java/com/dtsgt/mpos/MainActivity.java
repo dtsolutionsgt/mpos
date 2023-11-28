8dd4ff2be0e22222000651e5bc6c9819ca520090
ejcalderongt <ejcalderon02@yahoo.com>
2023-11-28 15:31:27 -0600

    4.8.21: Agregué funcionalidad para pequeño contribuyente XML don taco.

--- app/src/main/java/com/dtsgt/mpos/MainActivity.java
+++ app/src/main/java/com/dtsgt/mpos/MainActivity.java
@@ -40,8 +40,6 @@ import android.widget.Toast;
 
 import androidx.core.app.ActivityCompat;
 
-import androidx.core.app.ActivityCompat;
-
 import com.dtsgt.base.AppMethods;
 import com.dtsgt.base.BaseDatosVersion;
 import com.dtsgt.base.clsClasses;
@@ -75,7 +73,7 @@ public class MainActivity extends PBase {
     private String cs1, cs2, cs3, barcode,epresult, usr, pwd;
     private int scrdim, modopantalla,fri=0;
 
-    private String  parVer = "4.8.20.0";
+    private String  parVer = "4.8.21";
     private boolean bloqueo_venta=false;
 
     private Typeface typeface;
