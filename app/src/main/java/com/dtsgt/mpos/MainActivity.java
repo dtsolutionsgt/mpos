cc74686c58ab171716ee61110079082d1ad119b0
dtsolutionsgt <dtsolutionsgt@gmail.com>
2024-01-11 06:42:36 -0600

    Respaldo 11/1 4.8

--- app/src/main/java/com/dtsgt/mpos/MainActivity.java
+++ app/src/main/java/com/dtsgt/mpos/MainActivity.java
@@ -73,7 +73,7 @@ public class MainActivity extends PBase {
     private String cs1, cs2, cs3, barcode,epresult, usr, pwd;
     private int scrdim, modopantalla,fri=0;
 
-    private String  parVer = "4.8.23.1";
+    private String  parVer = "4.8.23.2";
     private boolean bloqueo_venta=false;
 
     private Typeface typeface;
