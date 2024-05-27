59e3e135bfa2c8355802efb91ccc580ba098aa21
dtsolutionsgt <dtsolutionsgt@gmail.com>
2024-05-27 07:43:36 -0600

    Respaldo 27/5 5.4

--- app/src/main/java/com/dtsgt/mpos/Menu.java
+++ app/src/main/java/com/dtsgt/mpos/Menu.java
@@ -3221,9 +3221,11 @@ public class Menu extends PBase {
 				msgbox("No existe respaldo del ultimo cierre");return;
 			}
 
+			/*
 			writer.write("   ");writer.write("\r\n");
 			writer.write(" REIMPRESION CIERRE DIA  ");writer.write("\r\n");
 			writer.write("   ");writer.write("\r\n");
+			*/
 
             for (int i = 0; i < D_cierreObj.count; i++) {
                 writer.write(D_cierreObj.items.get(i).text);writer.write("\r\n");
