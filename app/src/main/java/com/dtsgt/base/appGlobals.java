c5a2d81e24746fa1127b3da5f91c4b898ee897f0
dtsolutionsgt <dtsolutionsgt@gmail.com>
2025-04-22 07:38:34 -0600

    Respaldo 22/04 4.9

--- app/src/main/java/com/dtsgt/base/appGlobals.java
+++ app/src/main/java/com/dtsgt/base/appGlobals.java
@@ -94,7 +94,7 @@ public class appGlobals extends Application {
     public Boolean peAgregarCombo,peComboLimite,peComboDet,peFactSinPropina,peRedondPropina;
 	public Boolean peVentaDomicilio,peVentaEntrega,peDomEntEnvio,peNoCerrarMesas,peActOrdenMesas;
 	public Boolean peCafeTicket,peNoEnviar,peUsaSoloBOF,peAcumDesc, peNumOrdCommandaVenta;
-	public Boolean peImpFactBT,peImpFactLan,peImpFactUSB,peNumOrdCentral,peCajaMesasManual;
+	public Boolean peImpFactBT,peImpFactLan,peImpFactUSB,peImpFactUSBSTAR,peNumOrdCentral,peCajaMesasManual;
 	public Boolean peFactPropinaAparte,pePrecu1015,peComandaVentaLAN,peReg4impr,peRepFormaSuper;
     public double  pePropinaPerc,pePropinaCarta,peDescMax;
 
