4fac408f225aea30111f3c81e4b2d4e08190fdb7
dtsolutionsgt <dtsolutionsgt@gmail.com>
2025-05-27 15:16:57 -0600

    Respaldo 20/05 550

--- app/src/main/java/com/dtsgt/mpos/ResCliente.java
+++ app/src/main/java/com/dtsgt/mpos/ResCliente.java
@@ -563,6 +563,83 @@ public class ResCliente extends PBase {
     //region Aux
 
     private boolean validaNIT(String N)  {
+        String P, C, s, NC;
+        int[] v = {0,0,0,0,0,0,0,0,0,0};
+        int j, mp, sum, d11, m11, r11, cn, ll;
+
+        if (N.isEmpty()) return false;
+        //if (NitValidadoInfile) return true;
+        //if (!N.contains("-")) return false;
+
+        try {
+            ll = N.length();
+            if (ll<5) return false;
+
+            if (!N.contains("-")) {
+                P = N.substring(0,ll-1);
+                C = N.substring(ll-1,ll);
+                N=P+"-"+C;
+            }
+
+            N=N.trim();
+            N=N.replaceAll(" ","");
+            if (N.isEmpty()) return false;
+
+            N=N.toUpperCase();
+            if (N.equalsIgnoreCase("CF")) N="C.F.";
+            if (N.equalsIgnoreCase("C/F")) N="C.F.";
+            if (N.equalsIgnoreCase("C.F")) N="C.F.";
+            if (N.equalsIgnoreCase("CF.")) N="C.F.";
+            if (N.equalsIgnoreCase("C.F.")) return true;
+
+            ll = N.length();
+            if (ll<5) return false;
+
+            P = N.substring(0,ll-2);
+            C = N.substring(ll-1, ll);
+
+            ll = ll - 1; sum = 0;
+
+            try {
+
+                for (int i = 0; i <ll-1; i++) {
+                    s =P.substring( i, i+1);
+                    j=Integer.parseInt(s);
+                    mp = ll + 1 - i-1;
+                    sum = sum + j * mp;
+                }
+
+                d11 =(int) Math.floor(sum/11);
+                m11 = d11 * 11;
+                r11 = sum - m11;
+                cn = 11 - r11;
+
+                if (cn == 10) s = "K"; else s=""+cn;
+
+                if (cn>10) {
+                    cn = cn % 11;
+                    s =""+cn;
+                }
+
+                NC = P+"-"+s;
+
+                if (N.equalsIgnoreCase(NC)) {
+                    return true;
+                } else {
+                    return false;
+                }
+            } catch (Exception e) {
+                return false;
+            }
+        } catch (Exception e){
+            addlog(new Object(){}.getClass().getEnclosingMethod().getName(),e.getMessage(),"");
+        }
+        return true;
+
+    }
+
+
+    private boolean validaNITOld(String N)  {
 
         if (N.isEmpty()) return false;
         if (NitValidadoInfile) return true;
