6159c686719ebebd254a0ea6e3d5e0ad86b6df33
dtsolutionsgt <dtsolutionsgt@gmail.com>
2025-07-23 16:36:03 -0600

    Respaldo 20/05 550

--- app/src/main/java/com/dtsgt/mpos/Venta.java
+++ app/src/main/java/com/dtsgt/mpos/Venta.java
@@ -954,7 +954,6 @@ public class Venta extends PBase {
             fbcallmode=3;
             gl.limcant=getDisp(prodid);
 
-
             if (!tipo.equalsIgnoreCase("M")) {
                 if (tipo.equalsIgnoreCase("P")) {
                     if (gl.limcant>0) {
@@ -978,6 +977,7 @@ public class Venta extends PBase {
         Cursor DT;
 
         txtBarra.setText("");
+
         txtBarra.requestFocus();
 
         try {
