2d2b49e5a4879a41a7bce2d2199670004d496e98
dtsolutionsgt <dtsolutionsgt@gmail.com>
2025-07-23 21:11:00 -0600

    Respaldo 20/05 550

--- app/src/main/java/com/dtsgt/base/clsClasses.java
+++ app/src/main/java/com/dtsgt/base/clsClasses.java
@@ -2156,16 +2156,6 @@ public class clsClasses {
         public long fechault;
     }
 
-    public class clsfbVersion {
-        public String actver;
-        public int  eid;
-        public String enombre;
-        public int  rid;
-        public String rnombre;
-        public int  sid;
-        public String snombre;
-    }
-
     public class clsT_mov_almacen {
         public String corel;
         public int almacen_origen;
