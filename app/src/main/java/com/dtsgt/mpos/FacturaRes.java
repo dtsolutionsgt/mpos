76c77fefc174a09c2cb100efac07aa847cde8722
dtsolutionsgt <dtsolutionsgt@gmail.com>
2025-08-06 14:13:18 -0600

    Respaldo mpos

--- app/src/main/java/com/dtsgt/mpos/FacturaRes.java
+++ app/src/main/java/com/dtsgt/mpos/FacturaRes.java
@@ -5329,7 +5329,33 @@ public class FacturaRes extends PBase {
 		}
 	}
 
-	private void askNotaEnvio() {
+	public void askNotaEnvio() {
+
+		try {
+
+			AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+			dialog.setTitle(R.string.app_name);
+			dialog.setMessage("¿Aplicar envio?");
+			dialog.setCancelable(false);
+			dialog.setPositiveButton("Sí", (dialog1, which) -> {
+				try {
+					checkPagoEnvio();;
+				} catch (Exception e) {
+					msgbox(new Object(){}.getClass().getEnclosingMethod().getName()+" . "+e.getMessage());
+				}
+			});
+
+			dialog.setNegativeButton("No", (dialog1, which) -> {
+				prevScreen(null);
+			});
+			dialog.show();
+
+		} catch (Exception ex) {
+			toast(ex.getMessage());
+		}
+	}
+
+	private void askNotaEnvioOrig() {
 
 		try {
 			ExDialog dialog = new ExDialog(this);
--- app/src/main/java/com/dtsgt/mpos/MainActivity.java
+++ app/src/main/java/com/dtsgt/mpos/MainActivity.java
@@ -123,7 +123,7 @@ public class MainActivity extends PBase {
     private boolean bloqueo_venta=false;
 
     // ***************************************
-    private String parVer = "5.6.1.2";
+    private String parVer = "5.6.1.3";
 
     //ver. 5.6. - registro de version actual en Firebase
 
